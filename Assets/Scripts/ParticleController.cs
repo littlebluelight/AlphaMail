0cbae170d12a8b398ceb43f716283621bddcfbde
ChimiChangle <andychang43@gmail.com>
2018-10-28 23:43:30 -0400

    Everything I have
    includes fixed sprites and particles

---
+++ Assets/Scripts/ParticleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public GameObject[] particles;
+    public bool groundPuff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameObject.GetComponent<PlayerController>().isGrounded && !groundPuff) {
+            groundPuff = true;
+        }
+
+        if (gameObject.GetComponent<PlayerController>().isGrounded && groundPuff)
+        {
+            foreach (GameObject system in particles)
+            {
+                Instantiate(system, transform.position, system.transform.rotation);
+            }
+            groundPuff = false;
+        }
+    }
+}
